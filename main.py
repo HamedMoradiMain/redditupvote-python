eb3f95cb85359dc2c366cb4c837d9a6a5a8479cf
unknown <hamdmrady63@gmail.com>
2023-01-20 03:04:36 +0430

    upvote bot

---
+++ main.py
@@ -0,0 +1,15 @@
+try:
+    import threading
+    import getpass
+    import sys
+    import praw
+except Exception as e:
+    print(e)
+class RedditBot:
+    def main(self):
+        pass
+    def run(self):
+        pass
+if __name__ == "__main__":
+    bot = RedditBot()
+    bot.run()
